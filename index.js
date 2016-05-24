ebefd32bff38c12f2b0ba733127e6d7bf0b61ac9
M.C. van den Boogaart <scola@kvdb.name>
2016-05-24 14:02:28 +0200

    Use factory methods

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const Decoder = require('./lib/transformer');
+const Encoder = require('./lib/transformer');
+const type = 'application/octet-stream';
+
+function decoder(options) {
+  return {
+    type,
+    create: () => new Decoder(options)
+  };
+}
+
+function encoder(options) {
+  return {
+    type,
+    create: () => new Encoder(options)
+  };
+}
+
 module.exports = {
-  type: 'application/octet-stream',
-  Decoder: require('./lib/transformer'),
-  Encoder: require('./lib/transformer')
+  type,
+  decoder,
+  encoder,
+  Decoder,
+  Encoder
 };
